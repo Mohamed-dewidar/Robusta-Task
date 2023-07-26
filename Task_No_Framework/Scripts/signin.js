fc46980c5837428ce0e9913bb3d8d255ab6cf99d
Mohamed-dewidar <khaledmo747@gmail.com>
2023-07-27 00:56:30 +0300

    signin request with XHR

--- Task_No_Framework/Scripts/signin.js
+++ Task_No_Framework/Scripts/signin.js
@@ -1,62 +1,38 @@
 var API_URL = "https://private-b2e6827-robustatask.apiary-mock.com";
-var API_PATH_SIGNUP = "/auth/register";
-
+var API_PATH_SIGNIN = "/auth/login";
 
 // Variables to store references to HTML elements
-let fullNameElement; // Input element for user's full name
-let fullNameErrElement; // element for fullname error message
-
 let emailElement; // Input element for user's email
 let emailErrElement; // element for email error message
-
-let userNameElement; // Input element for user's username
-let userNameErrElement; // element for username error message
 
 let passwordElement; // Input element for user's password
 let passwordErrElement; // element for password error message
 
-let confirmPasswordElemnt; // Input element for user's confirm password
-let confirmPasswordErrElemnt; // element for confirm password error message
-
-let signupBtn; // Sign Up button element
+let signinBtn; // Sign IN button element
+let signinErrElement;
 
 // script variables
-let fullNameError = false; // boolean to check if the fullname is invalid before submit
 let emailError = false; // boolean to check if the email is invalid before submit
-let userNameError = false; // boolean to check if the username is invalid before submit
 let passwordError = false; // boolean to check if the password is invalid before submit
-let confirmPasswordError = false; // boolean to check if the confirm password is invalid before submit
 
 // Wait for the page to load before running the script
 addEventListener("load", scriptBegins);
 
 // This function holds the script logic
 function scriptBegins() {
-  fullNameElement = document.querySelector("[data-fullname]");
-  fullNameErrElement = document.querySelector("[data-fullnameErr]");
-
   emailElement = document.querySelector("[data-email]");
   emailErrElement = document.querySelector("[data-emailErr]");
-
-  userNameElement = document.querySelector("[data-username]");
-  userNameErrElement = document.querySelector("[data-usernameErr]");
 
   passwordElement = document.querySelector("[data-password]");
   passwordErrElement = document.querySelector("[data-passwordErr]");
 
-  confirmPasswordElemnt = document.querySelector("[data-confirmPassword]");
-  confirmPasswordErrElemnt = document.querySelector(
-    "[data-confirmPasswordErr]",
-  );
-
-  signupBtn = document.querySelector("[data-signupBtn]");
+  signinBtn = document.querySelector("[data-signinBtn]");
+  signinErrElement = document.querySelector("[data-signinErr]")
 
   // Attach eventListeners to The elements
-  fullNameElement.addEventListener("blur", validateFullname);
   emailElement.addEventListener("blur", validateEmail);
-  userNameElement.addEventListener("blur", validateUsername);
   passwordElement.addEventListener("blur", validatePassword);
-  confirmPasswordElemnt.addEventListener("blur", validateConfirmPassword);
+  signinBtn.addEventListener("click", singinHandler);
 }
 
 /**
@@ -65,20 +41,6 @@ function scriptBegins() {
  * - Each function will check value according to regex
  * - <fieldname>Err variable is used to check before submting that all fields are valid
  */
-
-function validateFullname(event) {
-  let fullName = event.target.value.trim();
-
-  if (!validator.matches(fullName, /^[A-Za-z\s]+$/)) {
-    fullNameElement.classList.add("invalid");
-    fullNameErrElement.classList.remove("hide");
-    fullNameError = true;
-    return;
-  }
-  fullNameElement.classList.remove("invalid");
-  fullNameErrElement.classList.add("hide");
-  fullNameError = false;
-}
 
 function validateEmail(event) {
   let email = event.target.value.trim();
@@ -93,21 +55,6 @@ function validateEmail(event) {
   emailElement.classList.remove("invalid");
   emailErrElement.classList.add("hide");
   emailError = false;
-}
-
-function validateUsername(event) {
-  let userName = event.target.value.trim();
-
-  if (!validator.isAlpha(userName)) {
-    userNameElement.classList.add("invalid");
-    userNameErrElement.classList.remove("hide");
-    userNameError = true;
-    return;
-  }
-
-  userNameElement.classList.remove("invalid");
-  userNameErrElement.classList.add("hide");
-  userNameError = false;
 }
 
 function validatePassword(event) {
@@ -134,20 +81,6 @@ function validatePassword(event) {
   passwordElement.classList.remove("invalid");
   passwordErrElement.classList.add("hide");
   passwordError = false;
-}
-
-function validateConfirmPassword(event) {
-  let confirmPassword = event.target.value;
-
-  if (confirmPassword !== passwordElement.value) {
-    confirmPasswordElemnt.classList.add("invalid");
-    confirmPasswordErrElemnt.classList.remove("hide");
-    confirmPasswordError = true;
-    return;
-  }
-  confirmPasswordElemnt.classList.remove("invalid");
-  confirmPasswordErrElemnt.classList.add("hide");
-  confirmPasswordError = false;
 }
 
 /**
@@ -186,3 +119,50 @@ function isValidPassword(password) {
 
   return errMsgs;
 }
+
+function singinHandler(event) {
+  event.preventDefault();
+  console.log(emailError, passwordError);
+  if (emailError || passwordError) {
+    return;
+  }
+
+  // Show the loading spinner before making the XHR request
+  loadingSpinner.style.display = "block";
+
+  // Configure the XHR request
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", `${API_URL}${API_PATH_SIGNIN}`);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  const body = {
+    email: emailElement.value,
+    password: passwordElement.value,
+  };
+
+  xhr.onload = function () {
+    // Hide the loading spinner after the response returns
+    loadingSpinner.style.display = "none";
+
+    if (xhr.status === 200) {
+      const data = JSON.parse(xhr.responseText);
+
+    } else {
+        signinErrElement.classList.remove('hide')
+        setTimeout(() => {
+            signinErrElement.classList.add('hide')
+        }, 2000)
+    }
+  };
+
+  xhr.onerror = function () {
+    // Hide the loading spinner after the response returns
+    loadingSpinner.style.display = "none";
+    signinErrElement.classList.remove('hide')
+        setTimeout(() => {
+            signinErrElement.classList.add('hide')
+        }, 2000)
+  };
+
+  xhr.send(body);
+}
+
